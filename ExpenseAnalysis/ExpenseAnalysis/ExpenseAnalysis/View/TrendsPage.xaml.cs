ed328cdf57a5f3fdba22c7212b10f944183b8ee1
Harikrishnan N <harikrishnann@syncfusion.com>
2017-04-07 00:22:56 +0530

    Slide and demo for the c# Corner Conference is committed.

---
+++ ExpenseAnalysis/ExpenseAnalysis/ExpenseAnalysis/View/TrendsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.SfChart.XForms;
+using System;
+
+namespace ExpenseAnalysis
+{
+    public partial class TrendsPage
+    {
+        public TrendsPage()
+        {
+            InitializeComponent();
+            FastLineSeries.StrokeDashArray = new double[] { 20, 5 };
+        }
+
+        private void Axis_LabelCreated(object sender, ChartAxisLabelEventArgs e)
+        {
+            DateTime date = new DateTime();
+            date = date.AddDays(e.Position);
+            e.LabelContent = date.ToString("MMM") + "\n" + date.ToString(" yy");
+        }
+    }
+}
